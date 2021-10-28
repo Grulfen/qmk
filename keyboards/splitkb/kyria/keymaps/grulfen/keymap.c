4222ef96a17912745066cbe319ccd8880652dd20
Gustav Svensk <grulfen3@gmail.com>
2021-10-28 22:29:18 +0200

    move alt to different thumbkey

--- keyboards/splitkb/kyria/keymaps/grulfen/keymap.c
+++ keyboards/splitkb/kyria/keymaps/grulfen/keymap.c
@@ -54,15 +54,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  | [ {  |      |  |      |  ] } |   N  |   M  | ,  < | . >  | /  ? | RShift |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |      |      | LAlt/| LSft/| Nav  |  | Sym  | LSft/|AltGr/| Bksp | RGUI |
- *                        |      |      | Enter| Space|      |  |      | Space| Bksp |      |      |
+ *                        |      | LAlt | Enter| LSft/| Nav  |  | Sym  | LSft/| Bksp | AltGr| RGUI |
+ *                        |      |      |      | Space|      |  |      | Space|      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
      KC_TAB  ,   KC_Q ,  KC_W  ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,   KC_I ,   KC_O ,   KC_P , KC_LBRC,
      CTL_ESC ,   KC_A ,  KC_S  ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,   KC_K ,   KC_L , KC_SCLN,CTL_QUOT,
      KC_LSFT ,   KC_Z ,  KC_X  ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC, _______, _______, KC_RBRC, KC_N   ,   KC_M , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
-                                 _______, _______, ALT_ENT, SFT_SPC,  NAV   , SYM    , SFT_SPC, AGR_BSP, KC_BSPC, KC_RGUI
+                                 _______, KC_LALT,  KC_ENT, SFT_SPC,  NAV   , SYM    , SFT_SPC, KC_BSPC, KC_RALT, KC_RGUI
     ),
 
 /*
