a399cf1b831b365967ed48efbf5ed543461414cd
Gustav Svensk <grulfen3@gmail.com>
2021-11-03 18:52:10 +0100

    Move Sym layer to closer thumbkey

--- keyboards/splitkb/kyria/keymaps/grulfen/keymap.c
+++ keyboards/splitkb/kyria/keymaps/grulfen/keymap.c
@@ -58,15 +58,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  | [ {  |      |  |      |  ] } |   N  |   M  | ,  < | . >  | /  ? | RShift |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |      | LAlt |Enter/| LSft/| Nav  |  | Sym  | LSft/| Bksp/| AltGr| RGUI |
- *                        |      |      | Nav  | Space|      |  |      | Space| Sym  |      |      |
+ *                        |      | LAlt |Enter/| LSft/| Nav  |  | Sym  | LSft/| Sym  | AltGr| RGUI |
+ *                        |      |      | Nav  | Space|      |  |      | Space|      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
      KC_TAB  ,   KC_Q ,  KC_W  ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,   KC_I ,   KC_O ,   KC_P , KC_LBRC,
      CTL_ESC ,   KC_A ,  KC_S  ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,   KC_K ,   KC_L , KC_SCLN,CTL_QUOT,
      KC_LSFT ,   KC_Z ,  KC_X  ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC, _______, _______, KC_RBRC, KC_N   ,   KC_M , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
-                                 _______, KC_LALT, NAV_ENT, SFT_SPC,  NAV   , SYM    ,RSFT_SPC, SYM_BSP, KC_RALT, KC_RGUI
+                                 _______, KC_LALT, NAV_ENT, SFT_SPC,  NAV   , SYM    ,RSFT_SPC,     SYM, KC_RALT, KC_RGUI
     ),
 /*
  * Nav Layer: Media, navigation, F-keys
