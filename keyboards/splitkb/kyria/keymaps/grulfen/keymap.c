c923f2d946c5fe932c22ae368545f40b77537c93
Gustav Svensk <grulfen3@gmail.com>
2021-12-03 22:30:00 +0100

    combine snakecase and capsword to write UPPERCASE_UNDERLINES easier

--- keyboards/splitkb/kyria/keymaps/grulfen/keymap.c
+++ keyboards/splitkb/kyria/keymaps/grulfen/keymap.c
@@ -226,6 +226,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
         case CAPSWORD:
             if (record->event.pressed) {
                 enable_caps_word();
+                enable_xcase_with(KC_UNDS);
             }
             return false;
         case SNEKCASE:
