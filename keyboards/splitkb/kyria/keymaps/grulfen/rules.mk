0cfb442e69514fac30b80f7241f30a8e2a82b7b4
Gustav Svensk <grulfen3@gmail.com>
2022-01-19 21:34:01 +0100

    add tap dance for capsword and snakeword

--- keyboards/splitkb/kyria/keymaps/grulfen/rules.mk
+++ keyboards/splitkb/kyria/keymaps/grulfen/rules.mk
@@ -4,5 +4,6 @@ ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = no       # Disable keyboard RGB underglow
 
 COMBO_ENABLE = yes
+TAP_DANCE_ENABLE = yes
 
 SRC += features/casemodes.c
