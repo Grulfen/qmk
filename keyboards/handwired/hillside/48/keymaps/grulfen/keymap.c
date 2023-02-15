0620a4fc331fb422df692a36021f2864064a176a
Gustav Svensk <grulfen3@gmail.com>
2023-02-15 22:11:26 +0100

    hillside: move backspace to thumb

--- keyboards/handwired/hillside/48/keymaps/grulfen/keymap.c
+++ keyboards/handwired/hillside/48/keymaps/grulfen/keymap.c
@@ -88,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+------.                ,------+------+------+------+------+------+--------|
  * |        |   Z  |   X  |   C  |   V  |   B  |      |                |      |   N  |   M  | ,  < | . >  | /  ? |        |
  * `----------------------+------+------+------+------+------.  ,------+------+------+------+------+----------------------'
- *                        |      |Caps- | Nav  |Numbar|      |  |      | Space| Sym  |      |      |
+ *                        |      |Caps- | Nav  |Numbar|      |  |      | Space| Sym  | BSPC |      |
  *                        |      |Word  |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
@@ -96,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      _______ ,   SE_Q ,  SE_W  ,  SE_E  ,   SE_R ,   SE_T ,                                        SE_Y,   SE_U ,   SE_I ,   SE_O ,   SE_P , _______,
      _______ ,   SE_A ,  SE_S  ,  SE_D  ,   SE_F ,   SE_G ,                                        SE_H,   SE_J ,   SE_K ,   SE_L , SE_SCLN, _______,
      _______ ,   SE_Z ,  SE_X  ,  SE_C  ,   SE_V ,   SE_B , _______,                   _______,    SE_N,   SE_M , SE_COMM, SE_DOT , SE_SLSH, _______,
-                                _______ , TD_CPS ,   NAV  , NUMBAR , _______, _______,  KC_SPC,     SYM, _______, _______
+                                _______ , TD_CPS ,   NAV  , NUMBAR , _______, _______,  KC_SPC,     SYM, KC_BSPC, _______
     ),
 /*
  * Nav Layer: Media, navigation, F-keys
@@ -234,9 +234,6 @@ const key_override_t **key_overrides = (const key_override_t *[]) {
 // {{{ combos
 
 enum combo_events {
-  IO_BSPC,
-  RBRCCBR_BSPC,
-  HOMEEND_BSPC,
   KL_ENT,
   XC_ESC,
   WE_TAB,
@@ -253,9 +250,6 @@ enum combo_events {
 
 uint16_t COMBO_LEN = COMBO_LENGTH;
 
-const uint16_t PROGMEM io_combo[] = {KC_I, KC_O, COMBO_END};
-const uint16_t PROGMEM rbrccbrr_combo[] = {SE_RBRC, SE_RCBR, COMBO_END};
-const uint16_t PROGMEM homeend_combo[] = {KC_HOME, KC_END, COMBO_END};
 const uint16_t PROGMEM kl_combo[] = {KC_K, KC_L, COMBO_END};
 const uint16_t PROGMEM xc_combo[] = {KC_X, KC_C, COMBO_END};
 const uint16_t PROGMEM we_combo[] = {KC_W, KC_E, COMBO_END};
@@ -269,9 +263,6 @@ const uint16_t PROGMEM mcomma_combo[] = {KC_M, SE_COMM, COMBO_END};
 const uint16_t PROGMEM commadot_combo[] = {SE_COMM, SE_DOT, COMBO_END};
 
 combo_t key_combos[] = {
-    [IO_BSPC] = COMBO(io_combo, KC_BSPC),
-    [RBRCCBR_BSPC] = COMBO(rbrccbrr_combo, KC_BSPC),
-    [HOMEEND_BSPC] = COMBO(homeend_combo, KC_BSPC),
     [KL_ENT] = COMBO(kl_combo, KC_ENT),
     [XC_ESC] = COMBO(xc_combo, KC_ESC),
     [WE_TAB] = COMBO(we_combo, KC_TAB),
