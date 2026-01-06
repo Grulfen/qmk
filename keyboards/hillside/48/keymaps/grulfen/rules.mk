00020bf87404bab82bd63f3fbe244d19701deb1b
Gustav Svensk <grulfen3@gmail.com>
2026-01-06 22:19:47 +0100

    hillside: enable rgb

--- keyboards/hillside/48/keymaps/grulfen/rules.mk
+++ keyboards/hillside/48/keymaps/grulfen/rules.mk
@@ -1,7 +1,7 @@
 OLED_ENABLE = no
 OLED_DRIVER = SSD1306      # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enable the use of one or more encoders
-RGBLIGHT_ENABLE = no       # Disable keyboard RGB underglow
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 PIN_COMPATIBLE = elite_c
 OS_DETECTION_ENABLE = yes
 
